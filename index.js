f5669b146492713e5fdec8b3e14f07b51af7b88e
Joko Prabowo <jokoprabowo4550@gmail.com>
2024-10-11 01:04:52 +0700

    add endpoints to complete URL shortener microservice task

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const dns = require('node:dns')
 const cors = require('cors')
-
+const database = require('./config/database')
+const { URL } = require('url')
+const Url = require('./models/url')
+ 
 const app = express()
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded())
 
 app.use(express.static('public'));
 
@@ -28,6 +33,48 @@ app.get('/api/whoami', (req, res) => {
         software: req.headers['user-agent']
     })
     
+})
+
+app.get('/api/shorturl/:url',async (req, res) => {
+    let url = req.params.url
+    let data =await Url.findOne({
+        short_url: url
+    })
+    res.redirect(data.original_url)
+})
+
+app.post('/api/shorturl', (req, res, next) => {
+    let url = new URL(req.body.url)
+    dns.lookup(url.hostname, (err, address, family) => {
+        if(err){
+            req.url = null
+            next()
+        }        
+        req.url = url
+        next()
+    })
+}, async (req, res) => {
+    let url = req.url
+    if(url == null){
+        res.json({
+            error: 'invalid url'
+        })
+    }else{
+        let data
+        data = await Url.findOne({
+            original_url: url
+        })
+        if(!data){
+            data = await Url.create({
+                original_url: url,
+                short_url: Math.floor(Math.random() * 100000).toString()
+            })
+        }
+        res.json({
+            original_url: data.original_url,
+            short_url: data.short_url
+        })
+    }
 })
 
 app.get("/api/:date", (req, res) =>{
